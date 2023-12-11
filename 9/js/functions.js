b8c96abd318adc0bb6c0c79a923f33c9b1db43d6
Keks <keks@htmlacademy.ru>
2023-12-11 23:38:27 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/functions.js
@@ -0,0 +1,30 @@
+const checkLineLength = (line, maxLength) => (line.length <= maxLength)
+
+const checkPalindrome = function(line)
+{
+  const normalizedLine = line.replaceAll(' ', '').toLowerCase();
+  let newLine = '';
+  for(let i = normalizedLine.length-1; i>=0; i--)
+  {
+    newLine += normalizedLine.at(i);
+  }
+  if(newLine === normalizedLine){
+    return newLine === normalizedLine;
+  }
+  else{
+    return false;
+  }
+}
+
+const getNumbers = function(line)
+{
+  let numbers = '';
+  line = line.toString();
+  for(let i = 0; i < line.length; i++)
+  {
+    if(!Number.isNaN(parseInt(line[i], 10))){
+      numbers += line[i];
+    }
+  }
+  return parseInt(numbers, 10);
+}
