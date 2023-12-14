8d697a5368a967f176d1c787332faaff6dac4cc8
Kirill-Eltsov <kirpav03@gmail.com>
2023-12-14 12:40:08 +0500

    Добавление слайдера и эффектов изображения

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import {createPicturePosts} from './data.js';
 import {renderPictures} from './renderPictures.js';
 import './workingWithForm.js';
+import './workingWithFilter.js';
 
 renderPictures(createPicturePosts());
