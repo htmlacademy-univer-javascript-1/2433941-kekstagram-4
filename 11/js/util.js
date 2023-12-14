3afd92b3edf790f105b5a43b164e0d6714ba9180
Keks <keks@htmlacademy.ru>
2023-12-14 10:41:58 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/util.js
@@ -0,0 +1,11 @@
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+export{getRandomInteger, getRandomArrayElement, isEscapeKey};
